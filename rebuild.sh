17817ead8916cbd60461bd37675a51ffd4c12387
Andy <andynu@gmail.com>
2024-09-19 14:17:10 -0400

    Fix the titles in the two html files.

--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,7 @@
+# Generate html
 npx markmap-cli --no-open mlmap.md -o public/index.html
 npx markmap-cli --no-open tools.md -o public/tools.html
+
+# Fix titles
+sed -i 's/<title>Markmap<\/title>/<title>AI in Biology and Biocompuuting<\/title>/' public/index.html
+sed -i 's/<title>Markmap<\/title>/<title>Tools in Biology and Biocompuuting<\/title>/' public/tools.html
